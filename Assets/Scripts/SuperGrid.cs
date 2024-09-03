f87ef1af13b2fc6768f6910da5355056c7318255
Drew Stevens <astevens@ccom.unh.edu>
2024-09-03 16:03:01 -0400

    active cell shader with animation

--- Assets/Scripts/SuperGrid.cs
+++ Assets/Scripts/SuperGrid.cs
@@ -12,9 +12,12 @@ public class SuperGrid : MonoBehaviour
     Shader gridCellShader;
 
     [SerializeField]
+    Shader gridCellActiveShader;
+
+    [SerializeField]
     Color UNASSIGNED, ASSIGNED, INSPECTED, QC_ASSIGNED, COMPLETE;
 
-    private  GameObject[,] _superGridCells;
+    private static GameObject[,] _superGridCells;
 
     public int width = 1000;
     public int height = 100;
@@ -24,11 +27,7 @@ public class SuperGrid : MonoBehaviour
     public Actions action;
 
     Dictionary<SuperGridCellProperties.SurveyStatus, Material> statusMaterials;
-
-    private void Awake()
-    {
-        if (statusMaterials == null) CreateMaterials();
-    }
+    Dictionary<SuperGridCellProperties.SurveyStatus, Material> statusActiveMaterials;
 
     // Start is called before the first frame update
     void Start()
@@ -43,11 +42,14 @@ public class SuperGrid : MonoBehaviour
 
     public void Initialize()
     {
+        ClearGrid();
+
         _superGridCells = new GameObject[width, height];
 
         GameObject prefabSGC = superGridCellPrefab;
 
-        if (statusMaterials == null || statusMaterials.Count == 0) CreateMaterials();
+        CreateMaterials();
+        CreateActiveMaterials();
 
         for (int i = 0; i < width; ++i)
         {
@@ -71,11 +73,13 @@ public class SuperGrid : MonoBehaviour
                 newCell.transform.localPosition = new Vector3(i, j, props.zEstimate);
                 newCell.transform.localScale = Vector3.one * 0.9f;
 
-                if (props.status == SuperGridCellProperties.SurveyStatus.UNASSIGNED) newCell.GetComponentInChildren<MeshRenderer>().material = statusMaterials[SuperGridCellProperties.SurveyStatus.UNASSIGNED];
-                if (props.status == SuperGridCellProperties.SurveyStatus.ASSIGNED) newCell.GetComponentInChildren<MeshRenderer>().material = statusMaterials[SuperGridCellProperties.SurveyStatus.ASSIGNED];
-                if (props.status == SuperGridCellProperties.SurveyStatus.INSPECTED) newCell.GetComponentInChildren<MeshRenderer>().material = statusMaterials[SuperGridCellProperties.SurveyStatus.INSPECTED];
-                if (props.status == SuperGridCellProperties.SurveyStatus.QC_ASSIGNED) newCell.GetComponentInChildren<MeshRenderer>().material = statusMaterials[SuperGridCellProperties.SurveyStatus.QC_ASSIGNED];
-                if (props.status == SuperGridCellProperties.SurveyStatus.COMPLETE) newCell.GetComponentInChildren<MeshRenderer>().material = statusMaterials[SuperGridCellProperties.SurveyStatus.COMPLETE];
+                var matDict = Random.Range(0, 100) > 98 ? statusActiveMaterials : statusMaterials;
+
+                if (props.status == SuperGridCellProperties.SurveyStatus.UNASSIGNED) newCell.GetComponentInChildren<MeshRenderer>().material = matDict[SuperGridCellProperties.SurveyStatus.UNASSIGNED];
+                if (props.status == SuperGridCellProperties.SurveyStatus.ASSIGNED) newCell.GetComponentInChildren<MeshRenderer>().material = matDict[SuperGridCellProperties.SurveyStatus.ASSIGNED];
+                if (props.status == SuperGridCellProperties.SurveyStatus.INSPECTED) newCell.GetComponentInChildren<MeshRenderer>().material = matDict[SuperGridCellProperties.SurveyStatus.INSPECTED];
+                if (props.status == SuperGridCellProperties.SurveyStatus.QC_ASSIGNED) newCell.GetComponentInChildren<MeshRenderer>().material = matDict[SuperGridCellProperties.SurveyStatus.QC_ASSIGNED];
+                if (props.status == SuperGridCellProperties.SurveyStatus.COMPLETE) newCell.GetComponentInChildren<MeshRenderer>().material = matDict[SuperGridCellProperties.SurveyStatus.COMPLETE];
 
                 _superGridCells[i, j] = newCell;
             }
@@ -88,6 +92,9 @@ public class SuperGrid : MonoBehaviour
         {
             foreach (var cell in _superGridCells) { DestroyImmediate(cell); }
         }
+
+        foreach (var child in GetComponentsInChildren<SuperGridCell>())
+        { DestroyImmediate(child.gameObject); }
 
         _superGridCells = null;
     }
@@ -102,5 +109,28 @@ public class SuperGrid : MonoBehaviour
             [SuperGridCellProperties.SurveyStatus.QC_ASSIGNED] = new Material(gridCellShader) { color = QC_ASSIGNED, name = "QC_ASSIGNED" },
             [SuperGridCellProperties.SurveyStatus.COMPLETE] = new Material(gridCellShader) { color = COMPLETE, name = "COMPLETE" }
         };
+
+        statusMaterials[SuperGridCellProperties.SurveyStatus.UNASSIGNED].SetColor("_Color", UNASSIGNED);
+        statusMaterials[SuperGridCellProperties.SurveyStatus.ASSIGNED].SetColor("_Color", ASSIGNED);
+        statusMaterials[SuperGridCellProperties.SurveyStatus.INSPECTED].SetColor("_Color", INSPECTED);
+        statusMaterials[SuperGridCellProperties.SurveyStatus.QC_ASSIGNED].SetColor("_Color", QC_ASSIGNED);
+        statusMaterials[SuperGridCellProperties.SurveyStatus.COMPLETE].SetColor("_Color", COMPLETE);
+    }
+    void CreateActiveMaterials()
+    {
+        statusActiveMaterials = new()
+        {
+            [SuperGridCellProperties.SurveyStatus.UNASSIGNED] = new Material(gridCellActiveShader) { color = UNASSIGNED, name = "UNASSIGNED ACTIVE" },
+            [SuperGridCellProperties.SurveyStatus.ASSIGNED] = new Material(gridCellActiveShader) { color = ASSIGNED, name = "ASSIGNED ACTIVE" },
+            [SuperGridCellProperties.SurveyStatus.INSPECTED] = new Material(gridCellActiveShader) { color = INSPECTED, name = "INSPECTED ACTIVE" },
+            [SuperGridCellProperties.SurveyStatus.QC_ASSIGNED] = new Material(gridCellActiveShader) { color = QC_ASSIGNED, name = "QC_ASSIGNED ACTIVE" },
+            [SuperGridCellProperties.SurveyStatus.COMPLETE] = new Material(gridCellActiveShader) { color = COMPLETE, name = "COMPLETE ACTIVE" }
+        };
+
+        statusActiveMaterials[SuperGridCellProperties.SurveyStatus.UNASSIGNED].SetColor("_Color", UNASSIGNED);
+        statusActiveMaterials[SuperGridCellProperties.SurveyStatus.ASSIGNED].SetColor("_Color", ASSIGNED);
+        statusActiveMaterials[SuperGridCellProperties.SurveyStatus.INSPECTED].SetColor("_Color", INSPECTED);
+        statusActiveMaterials[SuperGridCellProperties.SurveyStatus.QC_ASSIGNED].SetColor("_Color", QC_ASSIGNED);
+        statusActiveMaterials[SuperGridCellProperties.SurveyStatus.COMPLETE].SetColor("_Color", COMPLETE);
     }
 }
